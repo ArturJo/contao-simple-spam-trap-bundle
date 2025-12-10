2c78d5457d71e93fbdb4a4e12e005155bbf932d7
Artur Jozefiak <jozefiak@secra.de>
2025-12-10 17:05:51 +0000

    Update spam protection legend in translations to reference "solidwork"

--- src/Resources/contao/languages/de/tl_form.php
+++ src/Resources/contao/languages/de/tl_form.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_form']['simple_spam_legend'] = 'Spam-Schutz';
+$GLOBALS['TL_LANG']['tl_form']['simple_spam_legend'] = 'Spam-Schutz by solidwork';
 $GLOBALS['TL_LANG']['tl_form']['enable_time_spam'][0] = 'Zeitbasierten Spam-Schutz aktivieren';
 $GLOBALS['TL_LANG']['tl_form']['time_spam_seconds'][0] = 'Zeit (Sekunden)';
 $GLOBALS['TL_LANG']['tl_form']['enable_honeypot_spam'][0] = 'Honeypot aktivieren';
